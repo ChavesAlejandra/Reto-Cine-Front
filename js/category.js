72c9e14e827d0ce7d2a23b0afa29ede8e26f16a5
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-11-14 17:11:10 +0100

    first commit

---
+++ js/category.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const categoryTitle = document.getElementsByClassName('films_title');
+
+    fetchData();
+})
+
+/* FETCH */
+const fetchData = () => {}
